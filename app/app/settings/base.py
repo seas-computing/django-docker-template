6fc636d13ba4eb4aeb695ac47db00a1eabf62338
Chris Rogers <chrxr@outlook.com>
2021-03-23 11:51:47 -0400

    updated base.py to match .env file

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": environ.get("POSTGRES_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": environ.get("POSTGRES_DB", path.join(BASE_DIR, "db.sqlite3")),
-        "USER": environ.get("POSTGRES_USER", "user"),
-        "PASSWORD": environ.get("POSTGRES_PASSWORD", "password"),
+        "ENGINE": environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": environ.get("SQL_DATABASE", path.join(BASE_DIR, "db.sqlite3")),
+        "USER": environ.get("SQL_USER", "user"),
+        "PASSWORD": environ.get("SQL_PASSWORD", "password"),
         "HOST": environ.get("SQL_HOST", "localhost"),
         "PORT": environ.get("SQL_PORT", "5432"),
     }
